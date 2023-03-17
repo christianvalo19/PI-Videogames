1535c304d142f260177b2421fd90c9436bf65471
Christian Valo <christianvalo@hotmail.com>
2023-03-17 08:01:53 -0500

    creando modelos y relaciones

--- api/src/db.js
+++ api/src/db.js
@@ -6,6 +6,7 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
+
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
@@ -30,12 +31,14 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Videogame } = sequelize.models;
+const { Videogame, Genre } = sequelize.models;
 
+Videogame.belongsToMany(Genre, { through: 'videogame_genre' });
+Genre.belongsToMany(Videogame, { through: 'videogame_genre' });
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-  conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
+  sequelize,     // para importart la conexión { conn } = require('./db.js');
 };
--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,50 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id: {
+     type: DataTypes.UUID,
+     primaryKey: true,
+     defaultValue: DataTypes.UUIDV4
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    
+    platforms: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false,
+    },
+
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    releasedAt: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+
+    rating: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+
+    created: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+      },
+
+    dietsrecipe: {
+       type: DataTypes.ARRAY(DataTypes.STRING),
+       allowNull: false,
+       }  
+  },
+  {timestamps: false}
+  );
 };
